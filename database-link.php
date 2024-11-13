525cf710990ec2621e447d7dd64d1d4703623216
chaklader17 <aschaklader@gmail.com>
2024-11-14 00:24:22 +0600

    link with updated databasee

--- database-link.php
+++ database-link.php
@@ -3,7 +3,7 @@
 $sName = "localhost";
 $uName = "root";
 $pass  = "";
-$db_name = "office_management_database";
+$db_name = "office_management";
 
 try {
 	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
