5641e66ab82e94f7ac5a0d6481fb32ce1772f72e
ahnafim <ahnafim2001@gmail.com>
2024-11-27 01:11:53 +0600

    Update database-link.php

--- database-link.php
+++ database-link.php
@@ -3,7 +3,7 @@
 $sName = "localhost";
 $uName = "root";
 $pass  = "";
-$db_name = "office_management";
+$db_name = "office_management_database";
 
 try {
 	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
